14f950c6a80ca4c3574b75c490d4ce824506be11
Cosmin <cosmin.radu.craciun@gmail.com>
2023-02-28 18:41:54 +0100

    Initial commit

---
+++ LegislatieJustTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using FreeWebService;
+
+Console.WriteLine("Testing Legislatie Just endpoint!");
+
+var _soapClient = new FreeWebServiceClient();
+
+try{
+    var token = await _soapClient.GetTokenAsync();
+    Console.WriteLine($"Successfully received token {token}");
+} catch(Exception ex){
+    Console.WriteLine($"Failed in receiving token. Errpr: {ex.Message}");
+    throw;
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # LegislatieJustTest
+
+This is a simple project that tests the endpoint for legislatie just.
+
+`dotnet build` for building project.
+`dotnet run --project LegislatieJustTest` for running the project.
